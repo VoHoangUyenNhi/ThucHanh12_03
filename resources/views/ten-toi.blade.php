38553da80ec973b8896f0364c919d61f95907423
PhNgAnhThu <anhthuphng05@gmail.com>
2025-03-12 15:17:25 +0700

    ten cua anh thu ne

---
+++ resources/views/ten-toi.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang của tôi</title>
+</head>
+<body>
+    <h1 style="text-align: center; margin-top: 50px;">Tên của tôi là: <b>Phạm Nguyễn Anh Thư</b></h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/ten-cua-toi', function () {
+    return view('ten-toi');
+});
 
 require __DIR__.'/auth.php';
